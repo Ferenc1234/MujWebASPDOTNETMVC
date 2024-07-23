5fb935831fcacca97b02ac3f74025d7f78720858
Ferenc1234 <frubacek@boacraft.eu>
2024-07-23 11:32:53 +0200

    Added calculator microapp

---
+++ MujWeb/Controllers/CalculatorController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using MujWeb.Models;
+using System;
+
+
+namespace MujWeb.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Calculator obj)
+        {
+            try
+            {
+
+                switch (obj.Operation) // 1 = +, 2 = -, 3 = *, 4 = /
+                {
+                    case 1:
+                        obj.Result = Convert.ToDouble(obj.FirstNumber.Replace('.', ',')) + Convert.ToDouble(obj.SecondNumber.Replace('.', ','));
+                        break;
+                    case 2:
+                        obj.Result = Convert.ToDouble(obj.FirstNumber.Replace('.', ',')) - Convert.ToDouble(obj.SecondNumber.Replace('.', ','));
+                        break;
+                    case 3:
+                        obj.Result = Convert.ToDouble(obj.FirstNumber.Replace('.', ',')) * Convert.ToDouble(obj.SecondNumber.Replace('.', ','));
+                        break;
+                    case 4:
+                        obj.Result = Convert.ToDouble(obj.FirstNumber.Replace('.', ',')) / Convert.ToDouble(obj.SecondNumber.Replace('.', ','));
+                        break;
+                    default:
+                        obj.Result = 0;
+                        break;
+                }
+
+            }
+            catch 
+            {
+                ViewData["Error"] = "An error occured";    
+            }
+
+            ViewBag.Result = obj.Result;
+            return View();
+        }
+
+    }
+}
---
+++ MujWeb/Models/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MujWeb.Models
+{
+    public class Calculator
+    {
+        [DisplayName("První číslo")]
+        [Required]
+        public required string FirstNumber { get; set; }
+        [Required]
+        [DisplayName("Druhé číslo")]
+        public required string SecondNumber { get; set; }
+        [DisplayName("Výsledek")]
+        public double Result { get; set; }
+        public int Operation { get; set; } // 1 = +, 2 = -, 3 = *, 4 = /
+    }
+}
